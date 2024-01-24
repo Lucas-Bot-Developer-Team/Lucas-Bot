5dd22532bd209a5ff05e5f2c4058f6251daea57d
Lucas8485 <lucas@yydk77.cn>
2024-01-24 22:43:47 +0800

    fix: small problem

---
+++ Lucas-Bot-OneBot/Core/CommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Lucas_Bot_OneBot.Entities;
+
+namespace Lucas_Bot_OneBot.Core;
+
+public enum CommandHandlerType
+{
+    GROUP_ONLY,
+    PRIVATE_ONLY,
+    GROUP_AND_PRIVATE
+}
+
+internal record struct CommandHandler(string Name, Action<Command> Handler, CommandHandlerType Type);
---
+++ Lucas-Bot-OneBot/Modules/Amusement/YiYan/YiYanRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+
+namespace Lucas_Bot_OneBot.Modules.Amusement.YiYan;
+
+public class YiYanRenderer
+{
+    public static byte[] RenderEssenceMessage()
+    {
+        throw new NotImplementedException();
+    }
+}
